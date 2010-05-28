9c3ad6bbb048908b15d7ba84472b9d1190bf9843
Reuben Thomas <rrt@sc3d.org>
2010-05-28 12:09:41 +0100

    Remove debugging statements.

--- src/buffer.lua
+++ src/buffer.lua
@@ -2,7 +2,6 @@
 function copy_text_block (pt, size)
   local lp = pt.p
   local s = string.sub (lp.text, pt.o) .. "\n"
-  io.stderr:write ("copy_text_block " .. tostring (lp) .. " " .. s .. "\n")
 
   lp = lp.next
   while #s < size do
@@ -10,6 +9,5 @@ function copy_text_block (pt, size)
     lp = lp.next
   end
 
-  io.stderr:write ("returning " .. tostring (size) .. " " .. string.sub (s, 1, size) .. "\n")
   return string.sub (s, 1, size)
 end
