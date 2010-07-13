2dd870f1f6381eba1c5792f32a6d4e7c7dfdbc91
Reuben Thomas <rrt@sc3d.org>
2010-07-13 12:41:50 +0100

    Bump copyright date.

--- src/mkvars.lua
+++ src/mkvars.lua
@@ -1,6 +1,6 @@
 -- Produce dotzile.sample
 --
--- Copyright (c) 2006, 2007, 2009 Free Software Foundation, Inc.
+-- Copyright (c) 2006, 2007, 2009, 2010 Free Software Foundation, Inc.
 --
 -- This file is part of GNU Zile.
 --
