4a24915bab8609e4f080448661a9ed59352f6259
Reuben Thomas <rrt@sc3d.org>
2011-02-24 14:48:49 +0000

    Bump copyright year.

--- src/file.lua
+++ src/file.lua
@@ -1,6 +1,6 @@
 -- Disk file handling
 --
--- Copyright (c) 2009, 2010 Free Software Foundation, Inc.
+-- Copyright (c) 2009, 2010, 2011 Free Software Foundation, Inc.
 --
 -- This file is part of GNU Zile.
 --
